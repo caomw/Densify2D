c02de51ea90c8bc83f1f7e19bca61f437b2a89a2
alexhagiopol <alexhagiopol@gmail.com>
2016-03-28 16:41:02 -0400

    Triple point algorithm complete.

--- main.cpp
+++ main.cpp
@@ -33,7 +33,8 @@ double convertScale(double in, double inMin, double inMax, double outMin, double
     return (in + abs(inMin))*(outMax - outMin)/(inMax - inMin) - abs(outMin);
 }
 
-void makeSparseImage(Mat& colorImage, Mat& zImage, const string& pointsFileName){
+//point image
+void makeSparseImage(Mat& colorImage, Mat& pointImage, const string& pointsFileName){
     int rows = colorImage.rows;
     int cols = colorImage.cols;
     vector<SFMPoint> points;
@@ -52,20 +53,11 @@ void makeSparseImage(Mat& colorImage, Mat& zImage, const string& pointsFileName)
         int yPixelCoord = round(convertScale(points[pointIndex].location().y(),yMin,yMax,0,rows));
         Vec3b color = points[pointIndex].color();
         colorImage.at<Vec3b>(Point(xPixelCoord,yPixelCoord)) = color;
-        Vec3b height(points[pointIndex].location().z()*-1,points[pointIndex].location().z()*-1,points[pointIndex].location().z()*-1);
-        zImage.at<Vec3b>(Point(xPixelCoord,yPixelCoord)) = height;
+        Vec3d point(points[pointIndex].location().x(),points[pointIndex].location().y(),points[pointIndex].location().z());
+        pointImage.at<Vec3d>(Point(xPixelCoord,yPixelCoord)) = point;
     }
 }
-/*
-tuple<Point,Point,Point> searchForThreeNearestNeighbors((const Mat& queryImage, const Point& queryLocation, const int searchWindow){
-    tuple<Point,Point,Point> neighbors;
 
-    int startXIndex = queryLocation.x - searchWindow;
-    int startYIndex = queryLocation.y - searchWindow;
-    int endXIndex = queryLocation.x + searchWindow;
-    int endYIndex = queryLocation.y + searchWindow;
-}
-*/
 Point searchForNonZeroNeighbor(const Mat& queryImage, const Point& queryLocation, const int searchWindow){
     Point neighbor = Point(-1,-1); //return a negative location if we don't find a good candidate.
     int startXIndex = queryLocation.x - searchWindow;
@@ -95,8 +87,90 @@ Point searchForNonZeroNeighbor(const Mat& queryImage, const Point& queryLocation
             neighbor = candidate;
         }
     }
-    cout << "query location = " << queryLocation << " neighbor location  = " << neighbor << endl;
+    //cout << "query location = " << queryLocation << " neighbor location  = " << neighbor << endl;
     return neighbor;
+}
+
+tuple<Point,Point,Point> searchForThreeNearestNeighbors(const Mat& queryImage, const Point& queryLocation, const int searchWindow){
+    tuple<Point,Point,Point> neighbors;
+    Point firstNeighbor = Point(-1,-1);
+    Point secondNeighbor = Point(-1,-1);
+    Point thirdNeighbor = Point(-1,-1);
+    int startXIndex = queryLocation.x - searchWindow;
+    int startYIndex = queryLocation.y - searchWindow;
+    int endXIndex = queryLocation.x + searchWindow;
+    int endYIndex = queryLocation.y + searchWindow;
+    get<0>(neighbors) = firstNeighbor;
+    get<1>(neighbors) = secondNeighbor;
+    get<2>(neighbors) = thirdNeighbor;
+    if ((startXIndex < 0) || (endXIndex > queryImage.cols) || (startYIndex < 0) || (endYIndex > queryImage.rows)){
+        return neighbors; //return a negative location if we don't find a good candidate.
+    }
+    Vec3b zero = Vec3b(0,0,0);
+    std::vector<Point> candidateNeighbors;
+    for (int c = startXIndex; c < endXIndex; c++){
+        for (int r = startYIndex; r < endYIndex; r++){
+            Vec3b channelValue = queryImage.at<Vec3b>(Point(c,r));
+            if (channelValue != zero){
+                candidateNeighbors.push_back(Point(c,r));
+            }
+        }
+    }
+    if (candidateNeighbors.size() < 3) return neighbors; //return a negative location if we don't find 3 good candidates.
+    double minDistance1 = 9999;
+    double minDistance2 = 9999;
+    double minDistance3 = 9999;
+    cout << "QUERY LOCATION = " << queryLocation << endl;
+    for (int n = 0; n < candidateNeighbors.size(); n++){
+        Point candidate = candidateNeighbors[n];
+
+        double distance = sqrt(pow(candidate.x - queryLocation.x,2) + pow(candidate.y - queryLocation.y,2));
+        cout << "candidate #" << n << " = " << candidate.x << ","<< candidate.y << " distance = " << distance << endl;
+        if (distance < minDistance1){ //closest point
+            minDistance3 = minDistance2;
+            minDistance2 = minDistance1;
+            minDistance1 = distance;
+            get<2>(neighbors) = get<1>(neighbors);
+            get<1>(neighbors) = get<0>(neighbors);
+            get<0>(neighbors) = candidate;
+        } else if ((distance >= minDistance1) && (distance < minDistance2)){ //second closest point
+            minDistance3 = minDistance2;
+            minDistance2 = distance;
+            get<2>(neighbors) = get<1>(neighbors);
+            get<1>(neighbors) = candidate;
+        } else if ((distance >= minDistance1) && (distance >= minDistance2) && (distance < minDistance3)){ //third closest point
+            minDistance3 = distance;
+            get<2>(neighbors) = candidate;
+        }
+    }
+    cout << " neighbor 1 = " << get<0>(neighbors) << " neighbor 2 = " << get<1>(neighbors) << " neighbor 3 = " << get<2>(neighbors) << endl;
+    return neighbors;
+}
+
+Vec3b computePixelColorViaProjection(const Mat& pointImage, const Point& queryLocation, const tuple<Point,Point,Point>& neighbors, const std::vector<SimpleCamera>& cameras){
+    //convert OpenCV 3D point representation to GTSAM 3D point representation
+    Vec3d cvNeighbor1 = pointImage.at<Vec3d>(get<0>(neighbors));
+    Vec3d cvNeighbor2 = pointImage.at<Vec3d>(get<1>(neighbors));
+    Vec3d cvNeighbor3 = pointImage.at<Vec3d>(get<2>(neighbors));
+    Point3 gtsamNeighbor1(cvNeighbor1[0],cvNeighbor1[1],cvNeighbor1[2]);
+    Point3 gtsamNeighbor2(cvNeighbor2[0],cvNeighbor2[1],cvNeighbor2[2]);
+    Point3 gtsamNeighbor3(cvNeighbor3[0],cvNeighbor3[1],cvNeighbor3[2]);
+    //estimate world coordinates of query point
+    Point3 query3DPoint = (gtsamNeighbor1 + gtsamNeighbor2 + gtsamNeighbor3)/3; //compute average location in world
+
+    /*
+    //cout << "Query Location = " << queryLocation << endl;
+    cout << "Neighbor 1" << get<0>(neighbors) << endl;
+    cout << "Neighbor 2" << get<1>(neighbors) << endl;
+    cout << "Neighbor 3" << get<2>(neighbors) << endl;
+    gtsamNeighbor1.print("GTSAM Neighbor 1 \n");
+    gtsamNeighbor2.print("GTSAM Neighbor 2 \n");
+    gtsamNeighbor3.print("GTSAM Neighbor 3 \n");
+    query3DPoint.print("QUERY 3D POINT \n");
+    */
+
+    Vec3b color(0,0,0);
+    return color;
 }
 
 int main() {
@@ -105,24 +179,32 @@ int main() {
     const int searchWindow = 5;
     vector<SimpleCamera> cameras;
     vector<Mat> images;
+    getCamerasFromCSV(cameras,"Users/alexhagiopol/Densify2D/cameras.csv");
     Mat sparseColorImage(rows, cols, CV_8UC3, Scalar(0,0,0));
     Mat denseColorImage(rows, cols, CV_8UC3, Scalar(0,0,0));
-    Mat sparseZImage(rows, cols, CV_8UC3, Scalar(0,0,0));
-    makeSparseImage(sparseColorImage, sparseZImage, "/Users/alexhagiopol/Densify2D/points.csv");
+    Mat sparsePointImage(rows, cols, CV_32FC3, Scalar(0,0,0)); //use floats to store 3D points at each pixel
+    makeSparseImage(sparseColorImage, sparsePointImage, "/Users/alexhagiopol/Densify2D/points.csv");
     display(sparseColorImage, "SPARSE IMAGE", 1000);
     Vec3b zero = Vec3b(0,0,0);
     for (int r = 0; r < rows; r++){
         std::cout << "Computing row #" << r << std::endl;
         display(denseColorImage,"DENSE IMAGE",500);
         for (int c = 0; c < cols; c++){
-            Vec3b channelValue = sparseZImage.at<Vec3b>(Point(c,r));
-            if (channelValue == zero){
-                Point bestNeighbor = searchForNonZeroNeighbor(sparseZImage,Point(c,r),searchWindow);
-                if (bestNeighbor != Point(-1,-1)){
-                    denseColorImage.at<Vec3b>(Point(c,r)) = sparseColorImage.at<Vec3b>(bestNeighbor);
+            Vec3b channelValue = sparseColorImage.at<Vec3b>(Point(c,r));
+            if (channelValue == zero){ //For ever blank pixel, compute a best color to fill in.
+                //Point bestNeighbor = searchForNonZeroNeighbor(sparseZImage,Point(c,r),searchWindow);
+                tuple<Point,Point,Point> bestNeighbors = searchForThreeNearestNeighbors(sparseColorImage,Point(c,r),searchWindow);
+
+                Point bestNeighbor1 = get<0>(bestNeighbors);
+                Point bestNeighbor2 = get<1>(bestNeighbors);
+                Point bestNeighbor3 = get<2>(bestNeighbors);
+                if (get<0>(bestNeighbors) != Point(-1,-1)){ //ensure that good neighbors are found; if not leave pixel unchanged
+                    denseColorImage.at<Vec3b>(Point(c,r)) = (sparseColorImage.at<Vec3b>(bestNeighbor1)/3 + sparseColorImage.at<Vec3b>(bestNeighbor2)/3 + sparseColorImage.at<Vec3b>(bestNeighbor3)/3); //use average of 3 best neighbors
+                    //Vec3b assignedColor = computePixelColorViaProjection(sparsePointImage,Point(c,r),bestNeighbors,cameras);
                 }
-            } else{
-                denseColorImage.at<Vec3b>(Point(c,r)) = sparseColorImage.at<Vec3b>(Point(c,r));
+            } else{ //if the pixel is not blank, copy over its color into dense image
+                //cout << (double) channelValue.operator[](0) << " " << (double) channelValue.operator[](1) << " " <<  (double) channelValue.operator[](2) << endl;
+                denseColorImage.at<Vec3b>(Point(c,r)) = sparseColorImage.at<Vec3b>(Point(c,r)); //copy the color from the sparse image if we have it.
             }
         }
     }
